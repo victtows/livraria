82a8151d2707d6f36464663a0706debe1dbe8344
victtows <josevictorjustinosilva@gmail.com>
2024-10-17 10:38:20 -0300

    10. Incluindo relacionamento n para n no modelo do Livro

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,6 +1,7 @@
 from django.db import models
 from .categoria import Categoria
 from .editora import Editora
+from .autor import Autor
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
@@ -12,6 +13,9 @@ class Livro(models.Model):
     )
     editora = models.ForeignKey(
         Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True
+    )
+    autores = models.ManyToManyField(
+        Autor, related_name="livros", blank=True
     )
 
     def __str__(self):
