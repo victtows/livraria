fabdc45693e0d64b6c144dfc6c87858199d938f2
victtows <josevictorjustinosilva@gmail.com>
2024-10-17 11:08:01 -0300

    11. Modificação da API para Livro

--- core/views/livro.py
+++ core/views/livro.py
@@ -2,7 +2,16 @@ from rest_framework.viewsets import ModelViewSet
 
 from core.models import Categoria, Livro
 from core.serializers import CategoriaSerializer, LivroSerializer
+from core.serializers import LivroDetailSerializer, LivroSerializer
+from core.serializers import LivroListSerializer, LivroListSerializer
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+    
+    def get_serializer_class(self):
+        if self.action == "list":
+            return LivroListSerializer
+        elif self.action == "retrieve":
+            return LivroDetailSerializer
+        return LivroSerializer
