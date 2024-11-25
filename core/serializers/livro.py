f48807b23d7bd66c41b4941a13e233e67d1178d5
victtows <josevictorjustinosilva@gmail.com>
2024-11-25 09:39:07 -0300

    Customização do Admin.

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -33,17 +33,21 @@ class LivroRetrieveSerializer(ModelSerializer):
         depth = 1
 
 class LivroSerializer(ModelSerializer):
-    capa_attachment_key = SlugRelatedField(
-        source="capa",
-        queryset=Image.objects.all(),
-        slug_field="attachment_key",
-        required=False,
-        write_only=True,
-    )
-    capa = ImageSerializer(
-        required=False,
-        read_only=True
-    )
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
+        capa_attachment_key = SlugRelatedField(
+            source="capa",
+            queryset=Image.objects.all(),
+            slug_field="attachment_key",
+            required=False,
+            write_only=True,
+        )
+        capa = ImageSerializer(
+            required=False,
+            read_only=True
+        )
 
 class LivroListRetrieveSerializer(ModelSerializer):
     capa = ImageSerializer(required=False)  
